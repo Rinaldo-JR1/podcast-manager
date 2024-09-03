7f64909cde4df16960a0e34d6c57de3f223c76da
rinaldo-JR1 <rinaldo3uchoa@gmail.com>
2024-09-03 17:39:18 -0300

    Start of dependencies and infrasctructure

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,16 @@
-
-export function sayHelloWorld(world: string) {
-  return `Hello ${world}`;
+import express, { NextFunction, Request, Response } from "express";
+const app = express();
+let port: number;
+if (process.env.PORT) {
+  port = parseInt(process.env.PORT);
+} else {
+  throw new Error("Port is not defined");
 }
+
+app.use((req: Request, res: Response, next: NextFunction) => {
+  next();
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
